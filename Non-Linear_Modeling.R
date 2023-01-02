650b1a40c5b3340102ac6f3668033e0d94b55644
Miguel Roque <93716483+MiguelRoque23@users.noreply.github.com>
2023-01-02 16:23:51 +0000

    Update Non-Linear_Modeling.R

--- Non-Linear_Modeling.R
+++ Non-Linear_Modeling.R
@@ -1,9 +1,7 @@
 # Decision trees
 
 set.seed(155)
-
 train_kfold <- trainControl(method = 'cv', number = 5)
-
 grid <- expand.grid(maxdepth = (2:30))
 
 tree_d1 <- train(crimes ~ factor(year) + factor(month) + black + latino + asian + age + pop + hsol + nilf + male + log(area_kmsq) + pop_density + poverty, 
@@ -21,6 +19,28 @@ tree_d2 <- train(crimes ~ factor(year) + factor(month) + black + latino + pop + 
                tuneGrid = grid)
 
 print(tree_d2)
+
+tree_log_d1 <- train(log(crimes + 0.1) ~ factor(year) + factor(month) + black + latino + asian + age + pop + hsol + nilf + male + log(area_kmsq) + pop_density + poverty, 
+               data = data, 
+               method = 'rpart2', 
+               trControl = train_kfold, 
+               tuneGrid = grid)
+
+print(tree_log_d1)
+
+rmse(tree_log_d1$trainingData$crimes, exp(predict(tree_log_d1, tree_log_d1$trainingData)))
+mae(tree_log_d1$trainingData$crimes, exp(predict(tree_log_d1, tree_log_d1$trainingData)))
+
+tree_log_d2 <- train(log(crimes + 0.1) ~ factor(year) + factor(month) + black + latino + asian + age + pop + hsol + log(area_kmsq) + poverty, 
+               data = data, 
+               method = 'rpart2', 
+               trControl = train_kfold, 
+               tuneGrid = grid)
+
+print(tree_log_d2)
+
+rmse(tree_log_d2$trainingData$crimes, exp(predict(tree_log_d2, tree_log_d2$trainingData)))
+mae(tree_log_d2$trainingData$crimes, exp(predict(tree_log_d2, tree_log_d2$trainingData)))
 
 tree_u1 <- train(crimes ~ factor(year) + factor(month) + airport + bar + food + university + school + library + parking_lot + bank + hospital + healthcare_professional + pharmacy + social_center + theater + club + law_enforcement + fire_station + church + industrial + military + cemetery + park + indoor_sport + outdoor_sport + golf + office + station + liquor_store + food_and_beverages + mall + clothing + health_and_beauty + DIY + furniture + electronics + car_related_shops + art_and_hobbies + books_and_gifts + tourist_attraction + tourist_accommodation, 
                data = data, 
