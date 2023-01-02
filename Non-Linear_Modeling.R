62e039d1a3d69f12c958bdc4ee50a542449f1e23
Miguel Roque <93716483+MiguelRoque23@users.noreply.github.com>
2023-01-02 22:15:55 +0000

    Update Non-Linear_Modeling.R

--- Non-Linear_Modeling.R
+++ Non-Linear_Modeling.R
@@ -162,8 +162,8 @@ print(xgb_g2)
 
 set.seed(155)
 train_kfold <- trainControl(method = 'cv', number = 5)
-grid <- expand.grid(mtry = c(), 
-                    splitrule = 'gini', 
+grid <- expand.grid(mtry = c(3, 5, 7), 
+                    splitrule = 'variance', 
                     min.node.size = c(1, 3, 5))
 
 rf_d1 <- train(crimes ~ factor(year) + factor(month) + black + latino + asian + age + pop + hsol + nilf + male + log(area_kmsq) + pop_density + poverty, 
@@ -181,6 +181,28 @@ rf_d2 <- train(crimes ~ factor(year) + factor(month) + black + latino + pop + hs
                tuneGrid = grid)
 
 print(rf_d2)
+
+rf_log_d1 <- train(log(crimes + 0.1) ~ factor(year) + factor(month) + black + latino + asian + age + pop + hsol + nilf + male + log(area_kmsq) + pop_density + poverty, 
+               data = data, 
+               method = 'ranger', 
+               trControl = train_kfold, 
+               tuneGrid = grid)
+
+print(rf_log_d1)
+
+rmse(rf_log_d1$trainingData$crimes, exp(predict(rf_log_d1, rf_log_d1$trainingData)))
+mae(rf_log_d1$trainingData$crimes, exp(predict(rf_log_d1, rf_log_d1$trainingData)))
+
+rf_log_d2 <- train(log(crimes + 0.1) ~ factor(year) + factor(month) + black + latino + asian + age + pop + hsol + log(area_kmsq) + poverty, 
+               data = data, 
+               method = 'ranger', 
+               trControl = train_kfold, 
+               tuneGrid = grid)
+
+print(rf_log_d2)
+
+rmse(rf_log_d2$trainingData$crimes, exp(predict(rf_log_d2, rf_log_d2$trainingData)))
+mae(rf_log_d2$trainingData$crimes, exp(predict(rf_log_d2, rf_log_d2$trainingData)))
 
 rf_u1 <- train(crimes ~ factor(year) + factor(month) + airport + bar + food + university + school + library + parking_lot + bank + hospital + healthcare_professional + pharmacy + social_center + theater + club + law_enforcement + fire_station + church + industrial + military + cemetery + park + indoor_sport + outdoor_sport + golf + office + station + liquor_store + food_and_beverages + mall + clothing + health_and_beauty + DIY + furniture + electronics + car_related_shops + art_and_hobbies + books_and_gifts + tourist_attraction + tourist_accommodation, 
                data = data, 
